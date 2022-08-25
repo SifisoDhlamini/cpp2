772517465d4ec0d54734b6387bca86b47dbaf885
Scifi_the_dev <sifisoldhlamini@outlook.com>
2022-08-25 14:24:54 +0800

    added min and max element check on sets

--- CPP_STLs/sets/sets.cpp
+++ CPP_STLs/sets/sets.cpp
@@ -1,4 +1,5 @@
 #include <set>
+#include<algorithm>
 #include <iostream>
 
 using namespace std;
@@ -9,6 +10,8 @@ int main()
     s = {1, 2, 2, 3, 4, 4, 4, 5, 6, 7, 8, 8, 8, 8, 9, 10};
     cout << s.size() << endl;
     cout << s.max_size() << endl;
+    cout <<"MAX: "<<*max_element(s.begin(), s.end()) << endl;
+    cout <<"MIN: "<<*min_element(s.begin(), s.end()) << endl;
     s.insert(11);
     cout << "s: ";
     for (auto i : s)
